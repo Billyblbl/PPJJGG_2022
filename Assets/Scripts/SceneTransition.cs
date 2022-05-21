48912707096a72947268c308bba8f4dcc02cf2c6
Billyblbl <billyblbl.dev@gmail.com>
2022-05-21 17:34:22 +0200

    Added target scene validation in scene transition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -12,15 +12,30 @@ public class SceneTransition : MonoBehaviour {
 	public Gradient gradient = new();
 	public float fadeOutTime;
 	public string targetScene = string.Empty;
+	[HideInInspector] public int targetSceneIndex = -1;
+
+	private void OnValidate() {
+		var scene = SceneManager.GetSceneByName(targetScene);
+		if (scene.IsValid()) {
+			targetSceneIndex = scene.buildIndex;
+		} else {
+			targetSceneIndex = -1;
+			Debug.LogWarningFormat("Scene transition for {0} on {1} doesn't target a valid scene, scene will not transition", targetScene, gameObject.name);
+		}
+	}
 
 	float startTime;
 	private void OnEnable() {
+		if (targetSceneIndex < 0) {
+			Debug.LogWarningFormat("Scene transition for {0} on {1} doesn't target a valid scene, scene will not transition", targetScene, gameObject.name);
+			enabled = false;
+		}
 		startTime = Time.time;
 	}
 
 	private void Update() {
 		if (fade != null) fade.color = gradient.Evaluate(Mathf.InverseLerp(startTime, startTime + fadeOutTime, Time.time));
-		if (Time.time > startTime + fadeOutTime) SceneManager.LoadScene(targetScene);
+		if (Time.time > startTime + fadeOutTime) SceneManager.LoadScene(targetSceneIndex);
 	}
 
 }
