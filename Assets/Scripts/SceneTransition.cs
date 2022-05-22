69a3755fff924990fc9d5ba13e4afa7d9cdc25ff
Billyblbl <billyblbl.dev@gmail.com>
2022-05-22 21:18:57 +0200

    Level Design
    - Added Bookshelf model
    - Added Added banch model
    - Added boxes models
    - Increased monster speed
    - Renamed play scene to Hall
    - Added Library level
    - Added Storage level
    - Added custom editor to scene transition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,41 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 #nullable enable
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class SceneTransition : MonoBehaviour {
+
+	public const string NoScene = "None";
 
 	public Image? fade;
 	public Gradient gradient = new();
 	public float fadeOutTime;
-	public string targetScene = string.Empty;
-	[HideInInspector] public int targetSceneIndex = -1;
-
-	private void OnValidate() {
-		var scene = SceneManager.GetSceneByName(targetScene);
-		if (scene.IsValid()) {
-			targetSceneIndex = scene.buildIndex;
-		} else {
-			targetSceneIndex = -1;
-			Debug.LogWarningFormat("Scene transition for {0} on {1} doesn't target a valid scene, scene will not transition", targetScene, gameObject.name);
-		}
-	}
+	[HideInInspector] public string targetScene = NoScene;
 
 	float startTime;
 	private void OnEnable() {
-		if (targetSceneIndex < 0) {
-			Debug.LogWarningFormat("Scene transition for {0} on {1} doesn't target a valid scene, scene will not transition", targetScene, gameObject.name);
-			enabled = false;
-		}
+		if (targetScene == "None") enabled = false;
 		startTime = Time.time;
 	}
 
 	private void Update() {
 		if (fade != null) fade.color = gradient.Evaluate(Mathf.InverseLerp(startTime, startTime + fadeOutTime, Time.time));
-		if (Time.time > startTime + fadeOutTime) SceneManager.LoadScene(targetSceneIndex);
+		if (Time.time > startTime + fadeOutTime) SceneManager.LoadScene(targetScene);
 	}
 
 }
+
+
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(SceneTransition))] [CanEditMultipleObjects] public class SceneTransitionEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		EditorGUI.BeginChangeCheck();
+		var t = (target as SceneTransition)!;
+		var buildScenes = new List<string> {SceneTransition.NoScene};
+		buildScenes.AddRange(EditorBuildSettings.scenes.Select(scene => (scene.path)));
+		var index = (int)Mathf.Clamp(buildScenes.IndexOf(t.targetScene), 0, float.MaxValue);
+		var selected = EditorGUILayout.Popup("Target Scene", index, buildScenes.ToArray());
+		if (EditorGUI.EndChangeCheck()) {
+			Undo.RecordObject(t, "Update target scene");
+			t.targetScene = buildScenes[selected];
+		}
+	}
+
+}
+
+
+#endif
